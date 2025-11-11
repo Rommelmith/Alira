918da784576bc55bbb53629bc5ff9da1638dad57
Rommel Abbas <Rommelmith@users.noreply.github.com>
2025-11-12 00:52:36 +0500

    kb_operation completed

---
+++ kb_operation.py
@@ -0,0 +1,64 @@
+################ KB operations #####################
+import json
+import os
+import pickle
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+ITEMS = []
+IDS = []
+VECT = None
+MATRIX = None
+
+KB_JSON = "kb.json"
+
+def load_kb(path: str=KB_JSON):
+    global ITEMS
+    if os.path.exists(path):
+        with open(path, "r", encoding="utf-8") as f:
+            ITEMS = json.load(f)
+    else:
+        ITEMS = []
+    return ITEMS
+
+def load_or_build_index(items, cache_path="kbtfid.pkl", kb_path = KB_JSON):
+    global VECT, MATRIX, IDS
+    if os.path.exists(cache_path):
+        kb_time = os.path.getmtime(kb_path)
+        cache_time = os.path.getmtime(cache_path)
+        cache_fresh = cache_time >= kb_time
+
+    else:
+        cache_fresh = False
+
+
+    if cache_fresh:
+        try:
+            with open(cache_path, "rb") as f:
+                packed = pickle.load(f)
+            VECT = packed["vectorizer"]
+            MATRIX = packed["matrix"]
+            IDS = packed["ids"]
+            return
+        except Exception as e:
+            pass
+
+    else:
+        IDS = [item["id"] for item in items]
+        corps = [item["question"] + " " + item["answer"] for item in items]
+
+        VECT = TfidfVectorizer(
+            lowercase=True,
+            stop_words="english",
+            ngram_range=(1, 2),
+            max_features=5000
+        )
+        MATRIX = VECT.fit_transform(corps)
+        with open(cache_path, "wb") as f:
+            pickle.dump({"vectorizer": VECT, "matrix": MATRIX, "ids": IDS}, f)
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -1,13 +1,3 @@
-argument = (
-  'DC',
-  {'intents': [{'device': 'light', 'action': 'on'}, {'device': 'fan', 'action': 'on'}]},
-  {'DC': 0.95, 'KB': 0.4301, 'MACRO': 0.1, 'GPT': 0.2}
-)
-
-def anythingthis(decision,
-    payload,
-    scores):
-    print(decision)
-    print(payload["intents"])
-    print(scores)
-anythingthis(*argument)
+from kb_operation import load_kb, load_or_build_index
+items = load_kb()
+load_or_build_index(items)
